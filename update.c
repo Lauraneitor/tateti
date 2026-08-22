3d75eb1a04b614f14051ecf69999a55058b4283d
Lauraneitor <numen.systematum@gmail.com>
2026-08-22 09:37:12 -0300

    soluciona la superpocicion

--- update.c
+++ update.c
@@ -7,25 +7,63 @@
 bool update() {
   char entrada;
   scanf(" %c", &entrada);
+
   if ('1' == entrada) {
-    casilla1 = jugador_actual;
+    if (casilla1 == ' ') {
+      casilla1 = jugador_actual;
+    } else {
+      return false;
+    }
   } else if ('2' == entrada) {
-    casilla2 = jugador_actual;
+    if (casilla2 == ' ') {
+      casilla2 = jugador_actual;
+    } else {
+      return false;
+    }
   } else if ('3' == entrada) {
-    casilla3 = jugador_actual;
+    if (casilla3 == ' ') {
+      casilla3 = jugador_actual;
+    } else {
+      return false;
+    }
   } else if ('4' == entrada) {
-    casilla4 = jugador_actual;
+    if (casilla4 == ' ') {
+      casilla4 = jugador_actual;
+    } else {
+      return false;
+    }
   } else if ('5' == entrada) {
-    casilla5 = jugador_actual;
+    if (casilla5 == ' ') {
+      casilla5 = jugador_actual;
+    } else {
+      return false;
+    }
   } else if ('6' == entrada) {
-    casilla6 = jugador_actual;
+    if (casilla6 == ' ') {
+      casilla6 = jugador_actual;
+    } else {
+      return false;
+    }
   } else if ('7' == entrada) {
-    casilla7 = jugador_actual;
+    if (casilla7 == ' ') {
+      casilla7 = jugador_actual;
+    } else {
+      return false;
+    }
   } else if ('8' == entrada) {
-    casilla8 = jugador_actual;
+    if (casilla8 == ' ') {
+      casilla8 = jugador_actual;
+    } else {
+      return false;
+    }
   } else if ('9' == entrada) {
-    casilla9 = jugador_actual;
+    if (casilla9 == ' ') {
+      casilla9 = jugador_actual;
+    } else {
+      return false;
+    }
   }
+
   if (jugador_actual == 'X') {
     jugador_actual = 'O';
   } else {
