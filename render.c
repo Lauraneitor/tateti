a615595247e501c05f3e8f1471aa3755851a1b71
Lauraneitor <numen.systematum@gmail.com>
2026-08-29 10:03:46 -0300

    partir render

--- render.c
+++ render.c
@@ -4,12 +4,19 @@
 
 void render() {
   system("cls");
+  dibujar_tablero();
+  mostrar_resultado();
+}
+
+void dibujar_tablero() {
   printf(" %c | %c | %c \n", casilla1, casilla2, casilla3);
   printf("---|---|---\n");
   printf(" %c | %c | %c \n", casilla4, casilla5, casilla6);
   printf("---|---|---\n");
   printf(" %c | %c | %c \n", casilla7, casilla8, casilla9);
+}
 
+void mostrar_resultado() {
   if (ganador == ' ') {
     printf("\nJugador actual: %c\n", jugador_actual);
   } else if (ganador == '-') {
